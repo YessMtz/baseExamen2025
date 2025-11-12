7e6ab89f1e0c8420eb2fee765ce7b0bc42ff87fd
Daniela Sánchez <danielasanchez.itt@hotmail.com>
2025-11-12 12:38:56 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+<h1 align="center">⚡ Examen 1 ⚛️</h1>
+
+<p align="center">
+  <img src="https://vitejs.dev/logo.svg" alt="Vite Logo" width="100" />
+  <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt="React Logo" width="100" />
+</p>
+
+
+
+[Deploy de la aplicación](https://danielasanchez.github.io/wpyx5iwu/)
+
+
+<p align="center">
+  🚀 Proyectos creados con <strong>React JS</strong> utilizando <strong>Vite</strong> y <strong>CRA</strong> .  
+</p>
+
+
+## 🛠️ Instalación y ejecución para Vite
+
+```bash
+# Entrar a la carpeta ejemploVite
+cd ejemploVite
+
+# Instalar dependencias
+npm install
+
+# Ejecutar el servidor
+npm run dev
+```
+
+## 🛠️ Instalación y ejecución para CRA
+
+```bash
+# Entrar a la carpeta ejemplocra
+cd ejemplocra
+
+# Instalar dependencias
+npm install
+
+# Ejecutar el servidor
+npm start
+```
---
+++ ejemplocra/src/App.js
@@ -0,0 +1,55 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+/*
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Button } from 'react-bootstrap';
+
+<Button variant="secondary">Secondary</Button>
+
+
+  const productos=[
+        {codigo:1,descripcion:"Laptop HP", precio:11999},
+        {codigo:2,descripcion:"Apple Macbook Air", precio:18599},
+        {codigo:3,descripcion:"Pantalla Tcl", precio:7999},
+        {codigo:4,descripcion:"Cámara Seguridad", precio:411},
+        {codigo:5,descripcion:"Licuadora", precio:1799},
+        {codigo:6,descripcion:"Silla Gamer", precio:1499},
+  ];
+
+Header:
+background-color:#2b2728;
+
+Banner:
+background-color:#3c3c3c;
+
+Body:
+background-color:#ffffff14;
+
+*/
