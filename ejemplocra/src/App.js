9f0c6cdf3c374feefdacf5dde2ff5db7feca4f9f
Yessenia Martinez <105814773+YessMtz@users.noreply.github.com>
2025-12-03 13:55:05 -0800

    respaso

--- ejemplocra/src/App.js
+++ ejemplocra/src/App.js
@@ -1,23 +1,89 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Table from 'react-bootstrap/Table';
+import { Button } from 'react-bootstrap';
 
 function App() {
+const [productos, setProductos] = useState([
+    {codigo:1, descripcion:"Laptop HP", precio:11999},
+    {codigo:2, descripcion:"Apple Macbook Air", precio:18599},
+    {codigo:3, descripcion:"Pantalla Tcl", precio:7999},
+    {codigo:4, descripcion:"Cámara Seguridad", precio:411},
+    {codigo:5, descripcion:"Licuadora", precio:1799},
+    {codigo:6, descripcion:"Silla Gamer", precio:1499}
+  ]);
+
+            const[carrito,setCarrito]=useState([]);
+
+            const Agregar= (p) =>{
+              setCarrito([...carrito, p]);
+            }
+            const Eliminar= (p) =>{
+              const temporal = carrito.filter((pt, it, ot) => it=>it!==i);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          </header>
+
+          {
+          productos.map((p,i,o) =>
+          <h3>{p.codigo}</h3>)
+          }
+
+          
+          <Table striped bordered hover variant="dark">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Descripcion</th>
+          <th>Precio</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {productos.map((p,i,o)=>
+        <tr>
+          <td>{p.codigo}</td>
+          <td>{p.descripcion}</td>
+          <td>{p.precio}</td>
+          <td><Button onClick={()=>Agregar(p)} variant="secondary">+</Button></td>
+        </tr>
+        )
+        }
+        <tr>
+          
+        </tr>
+      </tbody>
+    </Table>
+    <Table striped bordered hover variant="dark">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Descripcion</th>
+          <th>Precio</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {productos.map((p,i,o)=>
+        <tr>
+          <td>{p.codigo}</td>
+          <td>{p.descripcion}</td>
+          <td>{p.precio}</td>
+          <td><Button onClick={()=>Eliminar(p)} variant="secondary">-</Button></td>
+        </tr>
+        )
+        }
+        <tr>
+          
+        </tr>
+      </tbody>
+    </Table>
+
+
+    
+          
     </div>
   );
 }
